2594afbac9c3220fde56e9c913b64e8c12a2ebda
Shaderax <rouzeval1998@live.fr>
2019-04-16 11:53:25 +0200

    Vulkan window not resizable.

--- Source/Graphics/GLFW/GLFWVulkan/GLFWVulkanHints.cpp
+++ Source/Graphics/GLFW/GLFWVulkan/GLFWVulkanHints.cpp
@@ -7,7 +7,8 @@ namespace Soon
 	void InitGLFWHints( void )
 	{
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+//		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	}
 }
