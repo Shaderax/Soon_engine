0bcb5aa31a4671626e41148add8587af119df6cc
Shaderax <rouzeval1998@live.fr>
2019-04-18 23:04:21 +0200

    Add Linux physical/logical device creation

--- Source/Core/Error.hpp
+++ Source/Core/Error.hpp
@@ -7,7 +7,30 @@ namespace Soon
 {
 	enum Error
 	{
-		S_SUCCESS,
-		S_ERROR
+		SN_SUCCESS,
+		SN_ERROR
+	};
+
+	template< bool ThrowIf >
+	void Error( const char *Function, const char *FullFilePath, int Line, const char* msg )
+	{
+		std::cout << "Soon Engine: " << (ThrowIf ? "Fatal Error" : "Error") << " in " << Function << "() (" << FullFilePath << ", " << Line << "): " << msg << std::endl;
+
+		if (ThrowIf)
+			throw std::runtime_error(std::string(msg));
 	}
+	
+#define SOON_ERR( condition, str )\
+do	\
+{	if (!condition)\
+    		Soon::Error<false>(__FUNCTION__, __FILE__, __LINE__, str); \
+} while(false)
+
+#define SOON_ERR_THROW( condition, str )\
+do	\
+{	\
+	if (!condition)\
+		Soon::Error<true>(__FUNCTION__, __FILE__, __LINE__, str); \
+} while(false)
+
 }
