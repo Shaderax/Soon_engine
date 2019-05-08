35531d167065349ceb7a83fb88e987ee13086b30
Nathan ROUZEVAL <nrouzeva@e1r2p22.42.fr>
2019-05-08 13:37:44 +0200

    Render System Entity Sort problem

--- Source/Graphics/GLFW/GLFWVulkan/GLFWVulkanRenderer.hpp
+++ Source/Graphics/GLFW/GLFWVulkan/GLFWVulkanRenderer.hpp
@@ -2,9 +2,22 @@
 
 class VulkanRenderer
 {
+	public:
+		BufferRenderer VulkanRenderer::AddToRender( uint32_t id )
+		{
+			BufferRenderer handler;
+
+			handler = GraphicsInstance::GetInstance()->CreateVertexBuffer(24);
+
+			_entityRenderer.push_back(handler);
+
+			return handler;
+		}
+
 	private:
 		struct EntityRenderer
 		{
+			Transform* tr;
 			std::vector< BufferRenderer > Components;
 		};
 
