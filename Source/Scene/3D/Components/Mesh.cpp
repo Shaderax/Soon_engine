8a801235ddf00c50fd6c17a74905179a86e9d69e
Nathan ROUZEVAL <nrouzeva@e3r7p21.42.fr>
2019-06-06 19:14:57 +0200

    TMP : Parser

--- Source/Scene/3D/Components/Mesh.cpp
+++ Source/Scene/3D/Components/Mesh.cpp
@@ -92,7 +92,7 @@ namespace Soon
 	void Mesh::LoadMesh(std::string path)
 	{
 		Assimp::Importer importer;
-		const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+//		const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 //
 //		if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) 
 //		{
