3c3bcfc2c85a81e168cea3bc0569d0021fcb926a
Nathan ROUZEVAL <nrouzeva@e3r1p9.42.fr>
2019-06-13 16:09:53 +0200

    Fix Index Buffer

--- Exemples/main_test.cpp
+++ Exemples/main_test.cpp
@@ -40,10 +40,10 @@ int main(int argc, char**argv)
 	Soon::Object Maya;
 	Maya.AddComponent<Soon::Mesh>();
 //	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Borderlands 2 - Maya/untitled.obj");
-//	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Borderlands 2 - Maya/maya.obj");
+	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Borderlands 2 - Maya/maya.obj");
 //	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Test/test.obj");
 //	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Test/cat.obj");
-	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Test/nanosuit.obj");
+//	Maya.GetComponent<Soon::Mesh>().LoadMesh("../Ressources/objects/Test/nanosuit.obj");
 
 	float inc = 0;
 //	Maya.GetComponent<Transform3D>().Rotate(0.0f, 90.0f, 0.0f);
